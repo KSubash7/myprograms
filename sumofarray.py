ee80d7971f37bb173756f0e0567d269882e16711
SUBASH.K <vh12116_it22@velhightech.com>
2025-01-31 07:16:40 +0530

    Create sumofarray.py

---
+++ sumofarray.py
@@ -0,0 +1,10 @@
+numbers = [5, 10, 15, 20, 25]
+total = 0
+for num in numbers:
+    total += num
+print("Sum of array elements:", total)
+numbers = [5, 10, 15, 20, 25]
+total = 0
+for num in numbers:
+    total += num
+print("Sum of array elements:", total)
